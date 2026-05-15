3f938f32777ebd2507efcb90e0dd68102ed65618
Zheila <loopy9923@gmail.com>
2026-05-15 23:16:44 +0900

    Remove temporary love visual cards

---
+++ remove-temp-love-visual-cards.js
@@ -0,0 +1,12 @@
+﻿const fs = require('fs');
+
+const file = 'public/report.html';
+let s = fs.readFileSync(file, 'utf8');
+
+s = s.replace(
+/\s*\/\/ === PART 요약 카드 ===[\s\S]*?\/\/ === LOVE_VISUAL_CARD_END ===\s*/g,
+'\n'
+);
+
+fs.writeFileSync(file, s, 'utf8');
+console.log('removed temporary love visual cards');
