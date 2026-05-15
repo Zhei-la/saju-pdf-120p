a099c8d0aa3ef1b02332542b03f09d006b0a2286
Zheila <loopy9923@gmail.com>
2026-05-15 20:36:14 +0900

    Rollback broken bonin cleanup

---
+++ rollback-bonin-cleanup.js
@@ -0,0 +1,16 @@
+﻿const fs = require('fs');
+
+const file = 'services/aiGenerator.js';
+let s = fs.readFileSync(file, 'utf8');
+
+s = s.replace(`.replace(/본인은 본인의/g, '본인의')
+        .replace(/본인은 본인을/g, '스스로를')
+        .replace(/본인은 상대/g, '상대')
+        .replace(/본인은 연애/g, '연애')
+        .replace(/본인은 관계/g, '관계')
+        .replace(/본인은/g, '')
+        .replace(/\\s{2,}/g, ' ')`, '');
+
+fs.writeFileSync(file, s, 'utf8');
+
+console.log('rolled back broken bonin cleanup');
