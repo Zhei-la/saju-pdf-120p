a1e900d9cae799e8ca56b1321ab981477cbb5f58
Zheila <loopy9923@gmail.com>
2026-05-15 21:01:56 +0900

    Fix shortName undefined error

---
+++ fix-shortname-undefined.js
@@ -0,0 +1,10 @@
+﻿const fs = require('fs');
+
+const file = 'services/aiGenerator.js';
+let s = fs.readFileSync(file, 'utf8');
+
+s = s.replace(/\.replace\(\/본인은\/g, shortName \+ '님은'\)\s*/g, '');
+s = s.replace(/\.replace\(\/본인의\/g, shortName \+ '님의'\)\s*/g, '');
+
+fs.writeFileSync(file, s, 'utf8');
+console.log('removed broken shortName replacement');
