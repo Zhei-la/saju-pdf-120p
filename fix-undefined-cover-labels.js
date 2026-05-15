51a95d3b9446e7f402132562e8b658ec7a89257a
Zheila <loopy9923@gmail.com>
2026-05-15 17:30:24 +0900

    Fix undefined PDF cover labels

---
+++ fix-undefined-cover-labels.js
@@ -0,0 +1,19 @@
+﻿const fs = require('fs');
+
+const file = 'public/report.html';
+let s = fs.readFileSync(file, 'utf8');
+
+s = s.replace(
+`  const totalChapters = currentChapters.length;
+  const estPages = isHalf ? 60 : 120;`,
+`  const totalChapters = currentChapters.length;
+  const reportLabel = currentUserInfo.reportType === 'love'
+    ? '연애운 집중 분석'
+    : '프리미엄 사주 분석';
+  const pageLabel = currentUserInfo.reportType === 'love'
+    ? '50페이지 내외'
+    : (isHalf ? '50페이지 내외' : '120페이지 내외');`
+);
+
+fs.writeFileSync(file, s, 'utf8');
+console.log('fixed undefined cover labels');
