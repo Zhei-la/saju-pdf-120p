3bbc8891b182896ddc573aeda67c608faaa3395e
Zheila <loopy9923@gmail.com>
2026-05-15 19:03:36 +0900

    Change report page label to 30 pages

---
+++ change-page-label-30p.js
@@ -0,0 +1,12 @@
+﻿const fs = require('fs');
+
+const file = 'public/report.html';
+let s = fs.readFileSync(file, 'utf8');
+
+s = s.replace(/50페이지 내외/g, '30페이지 내외');
+s = s.replace(/60페이지 내외/g, '30페이지 내외');
+s = s.replace(/약 60페이지/g, '30페이지 내외');
+s = s.replace(/약 50페이지/g, '30페이지 내외');
+
+fs.writeFileSync(file, s, 'utf8');
+console.log('page label changed to 30 pages');
