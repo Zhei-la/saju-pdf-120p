15243a6ba1074221a0c1b0d0c046ac0ee08e5707
Zheila <loopy9923@gmail.com>
2026-06-04 02:11:13 +0900

    Fix broken async declaration

---
+++ fix-async.js
@@ -0,0 +1,13 @@
+﻿const fs = require('fs');
+
+const file = 'public/u.html';
+let s = fs.readFileSync(file, 'utf8');
+
+s = s.replace(
+/async\s*\n/,
+'async function makeResult(){\n'
+);
+
+fs.writeFileSync(file, s, 'utf8');
+
+console.log('fixed async function declaration');
