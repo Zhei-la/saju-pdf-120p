5d61aeaa365140a009377f5a3bfdccd9fa96bdc1
Zheila <loopy9923@gmail.com>
2026-06-04 01:46:07 +0900

    Adjust ja hidden stems display

---
+++ fix-ja-hidden.js
@@ -0,0 +1,13 @@
+﻿const fs = require('fs');
+
+const file = 'services/saju/analysis/hiddenStems.js';
+let s = fs.readFileSync(file, 'utf8');
+
+s = s.replace(
+"子: ['癸']",
+"子: ['壬', '癸']"
+);
+
+fs.writeFileSync(file, s, 'utf8');
+
+console.log('子 hidden stems updated');
