9eebf61d302e3ab62c0f016ffa8d85369b13ec90
Zheila <loopy9923@gmail.com>
2026-06-04 01:34:59 +0900

    Remove spaces from hidden stems display

---
+++ fix-hidden-spacing.js
@@ -0,0 +1,28 @@
+﻿const fs = require('fs');
+
+const file = 'public/u.html';
+let s = fs.readFileSync(file, 'utf8');
+
+s = s.replace(
+/function stemListToKorean\(arr\)\{[\s\S]*?return \(arr \|\| \[\]\)\.map\(x => map\[x\] \|\| x\)\.join\(' '\);\s*\}/,
+`function stemListToKorean(arr){
+  const map = {
+    甲:'갑',
+    乙:'을',
+    丙:'병',
+    丁:'정',
+    戊:'무',
+    己:'기',
+    庚:'경',
+    辛:'신',
+    壬:'임',
+    癸:'계'
+  };
+
+  return (arr || []).map(x => map[x] || x).join('');
+}`
+);
+
+fs.writeFileSync(file, s, 'utf8');
+
+console.log('hidden stems spacing removed');
