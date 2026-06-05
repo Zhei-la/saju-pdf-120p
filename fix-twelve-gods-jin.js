7436b46afdddd0dfa40b2108d436cceae851c35f
Zheila <loopy9923@gmail.com>
2026-06-05 21:31:57 +0900

    Fix twelve gods for jin day branch

---
+++ fix-twelve-gods-jin.js
@@ -0,0 +1,14 @@
+﻿const fs = require('fs');
+
+const file = 'services/saju/analysis/twelveGods.js';
+let s = fs.readFileSync(file, 'utf8');
+
+s = s.replace("[B.JIN]:B.JA", "[B.JIN]:B.MYO");
+
+s = s.replace(
+"[B.MYO]:'장성살', [B.JIN]:'반안살', [B.SA]:'역마살', [B.O]:'육해살'",
+"[B.MYO]:'육해살', [B.JIN]:'반안살', [B.SA]:'역마살', [B.O]:'육해살'"
+);
+
+fs.writeFileSync(file, s, 'utf8');
+console.log('fixed twelve gods for jin day branch');
--- services/saju/analysis/twelveGods.js
+++ services/saju/analysis/twelveGods.js
@@ -1,52 +1,52 @@
-const twelveGodNames = [
-  '장성살',
-  '반안살',
-  '역마살',
-  '육해살',
-  '화개살',
-  '겁살',
-  '재살',
-  '천살',
-  '지살',
-  '년살',
-  '월살',
-  '망신살'
-];
-
-// 삼합 그룹 기준
-const groupStart = {
-  申: '子',
-  子: '子',
-  辰: '子',
-
-  寅: '午',
-  午: '午',
-  戌: '午',
-
-  亥: '卯',
-  卯: '卯',
-  未: '卯',
-
-  巳: '酉',
-  酉: '酉',
-  丑: '酉'
+﻿const B = {
+  JA: '\u5B50',
+  CHUK: '\u4E11',
+  IN: '\u5BC5',
+  MYO: '\u536F',
+  JIN: '\u8FB0',
+  SA: '\u5DF3',
+  O: '\u5348',
+  MI: '\u672A',
+  SIN: '\u7533',
+  YU: '\u9149',
+  SUL: '\u620C',
+  HAE: '\u4EA5'
 };
 
-const branchOrder = ['子','丑','寅','卯','辰','巳','午','未','申','酉','戌','亥'];
+const baseMap = {
+  [B.JA]: {
+    [B.JA]:'장성살', [B.CHUK]:'반안살', [B.IN]:'역마살', [B.MYO]:'육해살',
+    [B.JIN]:'화개살', [B.SA]:'겁살', [B.O]:'재살', [B.MI]:'천살',
+    [B.SIN]:'지살', [B.YU]:'년살', [B.SUL]:'월살', [B.HAE]:'망신살'
+  },
+  [B.O]: {
+    [B.O]:'장성살', [B.MI]:'반안살', [B.SIN]:'역마살', [B.YU]:'육해살',
+    [B.SUL]:'화개살', [B.HAE]:'겁살', [B.JA]:'재살', [B.CHUK]:'천살',
+    [B.IN]:'지살', [B.MYO]:'년살', [B.JIN]:'월살', [B.SA]:'망신살'
+  },
+  [B.MYO]: {
+    [B.MYO]:'육해살', [B.JIN]:'반안살', [B.SA]:'역마살', [B.O]:'육해살',
+    [B.MI]:'화개살', [B.SIN]:'겁살', [B.YU]:'재살', [B.SUL]:'천살',
+    [B.HAE]:'지살', [B.JA]:'년살', [B.CHUK]:'월살', [B.IN]:'망신살'
+  },
+  [B.YU]: {
+    [B.YU]:'장성살', [B.SUL]:'반안살', [B.HAE]:'역마살', [B.JA]:'육해살',
+    [B.CHUK]:'화개살', [B.IN]:'겁살', [B.MYO]:'재살', [B.JIN]:'천살',
+    [B.SA]:'지살', [B.O]:'년살', [B.MI]:'월살', [B.SIN]:'망신살'
+  }
+};
+
+const groupBase = {
+  [B.SIN]:B.JA, [B.JA]:B.JA, [B.JIN]:B.MYO,
+  [B.IN]:B.O, [B.O]:B.O, [B.SUL]:B.O,
+  [B.HAE]:B.MYO, [B.MYO]:B.MYO, [B.MI]:B.MYO,
+  [B.SA]:B.YU, [B.YU]:B.YU, [B.CHUK]:B.YU
+};
 
 function getTwelveGod(baseBranch, targetBranch) {
-  const startBranch = groupStart[baseBranch];
-
-  if (!startBranch) return '-';
-
-  const startIndex = branchOrder.indexOf(startBranch);
-  const targetIndex = branchOrder.indexOf(targetBranch);
-
-  if (startIndex === -1 || targetIndex === -1) return '-';
-
-  const diff = (targetIndex - startIndex + 12) % 12;
-
-  return twelveGodNames[diff] || '-';
+  const base = groupBase[baseBranch];
+  if (!base) return '-';
+  return (baseMap[base] && baseMap[base][targetBranch]) || '-';
 }
 
 function calcTwelveGods(dayBranch, pillars) {
