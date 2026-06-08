c6fff3dca7a009c3a4ba3a7c19df9ebc4ec83222
Zheila <loopy9923@gmail.com>
2026-06-08 17:02:13 +0900

    Set twelve gods basis to day branch

---
+++ set-twelve-basis-day.js
@@ -0,0 +1,12 @@
+﻿const fs = require('fs');
+
+const file = 'services/saju/index.js';
+let s = fs.readFileSync(file, 'utf8');
+
+s = s.replace(
+  "const twelveGods = calcTwelveGods(pillars, { basis: 'yearBranch' });",
+  "const twelveGods = calcTwelveGods(pillars, { basis: 'dayBranch' });"
+);
+
+fs.writeFileSync(file, s, 'utf8');
+console.log('twelveGods basis set to dayBranch');
