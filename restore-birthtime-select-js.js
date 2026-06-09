90f2b732c4107a40f46d2b1a6619587563bf61dd
Zheila <loopy9923@gmail.com>
2026-06-09 22:11:53 +0900

    Restore birth time select logic

---
+++ restore-birthtime-select-js.js
@@ -0,0 +1,12 @@
+﻿const fs = require('fs');
+
+const file = 'public/u.html';
+let s = fs.readFileSync(file, 'utf8');
+
+s = s.replace(
+/const birthTime\s*=\s*[\s\S]*?document\.getElementById\('birthTime'\)\.value;/,
+"const birthTime=document.getElementById('birthTime').value;"
+);
+
+fs.writeFileSync(file, s, 'utf8');
+console.log('birthTime JS restored to select value');
