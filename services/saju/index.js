79790e90c13241ffde519f57204ab830fc0f5781
Zheila <loopy9923@gmail.com>
2026-06-09 21:55:41 +0900

    Fix twelve gods basis to day branch

--- services/saju/index.js
+++ services/saju/index.js
@@ -518,7 +518,7 @@ const dayPillar =
   const tenGods = calcTenGods(dayPillar.stem, pillars, heavenlyStems);
   const hiddenStemsResult = calcHiddenStems(pillars);
   const twelveStates = calcTwelveStates(dayPillar.stem, pillars);
-  const twelveGods = calcTwelveGods(pillars, { basis: 'posteller' });
+  const twelveGods = calcTwelveGods(pillars, { basis: 'dayBranch' });
 
   const nowYear = new Date().getFullYear();
 
