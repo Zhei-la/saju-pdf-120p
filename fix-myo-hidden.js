1d8f78d79d61ec5070933900d233671781b3b633
Zheila <loopy9923@gmail.com>
2026-06-11 22:13:08 +0900

    Clean removed helper scripts

--- fix-myo-hidden.js
+++
@@ -1,9 +0,0 @@
-﻿const fs = require('fs');
-
-const file = 'services/saju/analysis/hiddenStems.js';
-let s = fs.readFileSync(file, 'utf8');
-
-s = s.replace("卯: ['乙']", "卯: ['甲', '乙']");
-
-fs.writeFileSync(file, s, 'utf8');
-console.log('卯 hidden stems changed to 甲乙');
